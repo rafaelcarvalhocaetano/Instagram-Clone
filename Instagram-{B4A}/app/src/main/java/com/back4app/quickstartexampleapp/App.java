3d3216ef2a95f83fad59d939d45a07452fada045
Rafael Carvalho Caetano <rapha.pse@outlook.com>
2017-12-22 14:46:43 -0200

    Aplicativo clone do instagram com back-and back{4}App

--- Instagram-{B4A}/app/src/main/java/com/back4app/quickstartexampleapp/App.java
+++ Instagram-{B4A}/app/src/main/java/com/back4app/quickstartexampleapp/App.java
@@ -27,21 +27,7 @@ public class App extends Application {
                 .build()
         );
 
-/*
-      // Teste de configuraÃ§Ã£o do App
-      ParseObject pontuacao = new ParseObject("Pontuacao");
-      pontuacao.put("pontos", 100);
-      pontuacao.saveInBackground(new SaveCallback() {
-          public void done(ParseException e) {
-              if (e == null) {
-                  Log.i("TesteExecucao", "Salvo com sucesso!!!");
-              } else {
-                  Log.i("TesteExecucao", "Falha ao salvar os dados!!!");
-              }
-          }
-      });
-
-    }
-    */
+        ParseACL defaultACL = new ParseACL();
+        ParseACL.setDefaultACL( defaultACL, true );
     }
 }
